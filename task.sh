6fea0bcc833feb7d82b7afa582293bda3cf89b70
Taras Kudinov <38283923+tkudinov@users.noreply.github.com>
2025-02-17 19:58:24 +0200

    Added task.sh file.
    Implemented bash script for database backup in several cases.

--- task.sh
+++ task.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+export DBUser = 'mysqluser';
+export  DBPassword = 'P@ssw0rd'
+
+mysqldump -u $DBUser -p $DBPassword --databases ShopDB --result-file=backup-shopdb.sql
+mysql -u $DBUser -p $DBPassword ShopDBReserve < backup-shopdb.sql
+
+mysqldump -u $DBUser -p $DBPassword --databases ShopDB --no-create-info --result-file=backup-dev.sql
+mysql -u $DBUser -p $DBPassword ShopDBDevelopment < backup-dev.sql
